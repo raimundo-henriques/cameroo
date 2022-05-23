59d302a3caad0e5f0e09b405e974c7fab038dd9e
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-05-23 15:38:00 +0100

    ARCH: devise user model created

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,9 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "cameroo_production"
 
+  config.action_mailer.default_url_options = { host: 'http://cameroo-rental.herokuapp.com' }
+
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
