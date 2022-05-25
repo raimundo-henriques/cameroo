be7629453b911e88c5aaa898eba4699e3fd9d863
Ricardo Silva <ricardosilva.rss@gmail.com>
2022-05-25 14:04:21 +0100

    still have an authorization error, going to merge with the master

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,9 +6,22 @@ class BookingsController < ApplicationController
   end
 
   def new
+    @booking = Booking.new
+    @item = Item.find(params[:item_id])
+    # authorize @booking
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.item = Item.find(params[:item_id])
+    # authorize @booking
+
+    if @booking.save
+      redirect_to item_bookings(@booking), notice: 'Booking was successfully.'
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +31,11 @@ class BookingsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
